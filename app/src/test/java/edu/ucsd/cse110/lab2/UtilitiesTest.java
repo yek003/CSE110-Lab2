ebcd8ce202d518a5a8c37bca4450be0cd1ee994e
Yerin Kim <yek003@ucsd.edu>
2023-01-20 14:16:54 -0800

    finished UtilitiesTest

--- app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class UtilitiesTest {
     @Test
     public void test_trimDisplayStr() {
-
-        assertEquals(3, 1 + 2);
+        assertEquals("1.234", Utilities.trimDisplayStr("1.2340"));
+        assertEquals("13", Utilities.trimDisplayStr(("13.0")));
     }
 }
